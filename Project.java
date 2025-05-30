23bb942fd5a5a0c66454bb3a0ada38ee5d5c2409
Hariharan <hola1201comrade1201@gmail.com>
2025-05-30 12:03:21 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -470,7 +470,7 @@ public class StudentMarksSystem {
         }
     }
     
-    private static void MarksOperations() throws SQLException {
+    private static void marksOperations() throws SQLException {
         boolean back = false;
         while (!back) {
             System.out.println("\nMarks Operations");
